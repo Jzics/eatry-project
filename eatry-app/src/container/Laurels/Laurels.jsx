2288b57fd649370da0b6faee10f8433f4f28a7b8
Jzics <jamesisaac87@gmail.com>
2023-04-05 14:41:30 +0100

    add the proper styling to the award section

--- eatry-app/src/container/Laurels/Laurels.jsx
+++ eatry-app/src/container/Laurels/Laurels.jsx
@@ -9,10 +9,10 @@ const AwardCard = ({award: {imgUrl, title, subtitle}}) => (
     <img src={imgUrl} alt='award' />
     <div className='app__laurels_awards-card_content'>
       <p className='p__cormorant' style={{color: '#dcca87'}}>{title}</p>
-      <p className='p__cormorant' style={{color: '#dcca87'}}>{title}</p>
+      <p className='p__cormorant'>{subtitle}</p>
     </div>
   </div>
-)
+);
 
 const Laurels = () => (
   <div className='app__bg app__wrapper section__padding' id='awards'>
@@ -20,13 +20,14 @@ const Laurels = () => (
       <SubHeading title='Awards & Recognition' />
       <h1 className='headtext__cormorant'>Our Laurels</h1>
 
-    <div className='app__laurels_awards'>
+      <div className='app__laurels_awards'>
       {data.awards.map((award) => <AwardCard award={award} key={award.title} />)}
+      </div>
     </div>
 
+    <div className='app__wrapper_img'>
+      <img src={images.laurels} alt='laurels' />
     </div>
-
-    <div className='app__wrapper_img'></div>
   </div>
 );
 
